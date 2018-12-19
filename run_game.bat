1bc16cc59998f23087fc39e37ca728075014afde
Alexander Jiang <dashuai99@gmail.com>
2018-12-18 23:17:34 -0600

    run_game script upgrade: randomly choose board size and don't echo all commands

--- run_game.bat
+++ run_game.bat
@@ -1,8 +1,27 @@
-REM This is a comment
-
-REM halite.exe --replay-directory replays/ -vvv --width 32 --height 32 --seed 1544749152 "python MyBotv3.py" "python MyBotv3_1.py"
+@echo off
 
 REM This seed isn't very halite dense, good for testing inspire-based or collide/steal strategies
 REM halite.exe --replay-directory replays/ -vvv --width 32 --height 32 --seed 1545020736 "python MyBotv3_1.py" "python MyBotv4.py"
 
-halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "python MyBotv4.py" "python MyBotv4_1.py"
+SET /A num=%RANDOM% * 5 / 32768
+GOTO :label%num%
+
+:label0
+halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "python MyBotv4_1.py" "python MyBotv4_2.py"
+EXIT /B
+
+:label1
+halite.exe --replay-directory replays/ -vvv --width 40 --height 40 "python MyBotv4_1.py" "python MyBotv4_2.py"
+EXIT /B
+
+:label2
+halite.exe --replay-directory replays/ -vvv --width 48 --height 48 "python MyBotv4_1.py" "python MyBotv4_2.py"
+EXIT /B
+
+:label3
+halite.exe --replay-directory replays/ -vvv --width 56 --height 56 "python MyBotv4_1.py" "python MyBotv4_2.py"
+EXIT /B
+
+:label4
+halite.exe --replay-directory replays/ -vvv --width 64 --height 64 "python MyBotv4_1.py" "python MyBotv4_2.py"
+EXIT /B
